9b65877abdfaa363ee9bf65338d2b53d44a62887
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-11-08 21:22:26 +0200

    create post place

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,44 +2,72 @@ import React, { useContext, useEffect, useState } from 'react'
 import { StoreContext } from './StoreProvider'
 import Loading from './Loading'
 import Navbar from './Navbar'
+import profile from '../public/user.png'
 
 
 export default function Home() {
 
-    const { getallPosts } = useContext(StoreContext)
+    const { getallPosts, getUserData } = useContext(StoreContext)
     const [allPosts, setAllPosts] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
+    const [userData, setUserData] = useState(null)
 
     const displayPosts = async () => {
         setIsLoading(true)
         let response = await getallPosts()
         console.log(response)
-        setAllPosts(response.data.posts)
+        const sortedPosts = response.data.posts.sort(
+            (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        )
+        setAllPosts(sortedPosts)
         setIsLoading(false)
     }
     useEffect(() => {
         displayPosts()
 
     }, [])
+    const displayUserData = async () => {
+        setIsLoading(true)
+        const response = await getUserData()
+        console.log(response)
+        setUserData(response.data.user)
+        setIsLoading(false)
+    }
+    useEffect(() => {
+        displayUserData()
+    }, [])
 
     return (
 
         isLoading ? (<Loading />) : (<>
-        <Navbar/>
+            <Navbar />
+
             <div className='bg-bg-hero  min-h-screen flex flex-col p-4 items-center gap-3'>
+                <div className='bg-white dark:bg-gray-950 h-16  max-w-md mx-auto w-full rounded-lg p-4 flex  items-center cursor-pointer'>
+                    <img className='w-10 h-10 rounded-full cursor-pointer object-cover object-top mr-3' src={userData?.photo ? userData.photo : profile} alt="" />
+                    <p className='text-gray-500 flex-1'>what's on your mind?</p>
+                   <i className="fa-solid fa-image text-green-500 fa-lg cursor-pointer" />
+
+                </div>
                 {allPosts?.slice(4).map((post, i) => (
                     <div key={i} className="container md:w-md  bg-white dark:bg-gray-950 rounded-xl shadow-md">
 
                         {/* Profile */}
-                        <div className="flex p-4 justify-between">
+                        <div className=" p-4 ">
                             <div className="flex items-center space-x-2">
                                 <img
                                     className="h-10 w-10 object-cover rounded-full"
                                     src={post.user.photo}
                                     alt="profile"
                                 />
-                                <h2 className="text-gray-800 dark:text-gray-100 font-bold">{post.user.name}</h2>
+                                <div>
+                                    <h2 className="text-gray-800 dark:text-gray-100 font-bold capitalize">{post.user.name}</h2>
+                                    <p className="text-gray-500 dark:text-gray-400 text-xs">
+                                        {new Date(post.createdAt).toLocaleString()}</p>
+                                </div>
+
                             </div>
+
                         </div>
 
                         {/* لو في صورة */}
