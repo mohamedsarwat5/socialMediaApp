6544e3742bb7b279c3740e63e212ade9e18978c0
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-11-19 21:55:45 +0200

    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,7 @@ import Loading from './Loading'
 import Navbar from './Navbar'
 import profile from '../public/user.png'
 import Stories from './Stories'
+import { Link } from 'react-router-dom'
 
 
 export default function Home() {
@@ -60,23 +61,50 @@ export default function Home() {
         });
         return `${formattedDate} ${formattedTime}`;
     }
+    const baseUrl = import.meta.env.VITE_BASE_URL
+    const [openModal, setOpenModal] = useState(false)
+    const handleOpenModal = () => setOpenModal(prev => !prev)
 
     return (
 
         isLoading ? (<Loading />) : (<>
-                <Navbar />
-            <div className='bg-bg-hero  min-h-screen flex flex-col p-4 items-center gap-3'>
+            <Navbar />
+            <div className='bg-bg-hero  min-h-screen flex flex-col p-4 items-center gap-3'
+            onClick={(e) => e.stopPropagation()}>
 
 
-                <div className='bg-white dark:bg-gray-950 h-16  max-w-lg mx-auto w-full rounded-lg p-4 flex  items-center cursor-pointer'>
-                    <img className='w-10 h-10 rounded-full cursor-pointer object-cover object-top mr-3' src={userData?.photo ? userData.photo : profile} alt="" />
-                    <p className='text-gray-500 flex-1'>what's on your mind?</p>
-                    <i className="fa-solid fa-image text-green-500 fa-lg cursor-pointer" />
+                <div className='bg-white dark:bg-gray-950 h-16  max-w-lg mx-auto w-full rounded-lg p-4 flex z-50 items-center cursor-pointer'>
+                    <Link to={`profile`}> <img className='w-10 h-10 rounded-full cursor-pointer object-cover object-top mr-3' src={userData?.photo ? userData.photo : profile} alt="" /></Link>
+                    <p onClick={handleOpenModal} className='text-gray-500 flex-1'>what's on your mind?</p>
+                    <button onClick={()=>handleOpenModal(true)}><i className="fa-solid fa-image text-green-500 fa-lg cursor-pointer" /></button>
+                </div>
+
+                <div className={`${openModal ? "flex z-50 " : "hidden z-0"} z-50  justify-center fixed inset-0 bg-black/80 transition-all duration-200`}>
+                    <div className={`bg-bg-hero rounded-2xl translate-y-16 relative h-fit max-w-lg w-full p-4`}>
+
+                        <div className='border-b header border-gray-400 pb-3 relative mb-3'>
+                            <h2 className='text-center font-bold text-text-color'>Create Post</h2>
+                            <button onClick={() => setOpenModal(false)} className='absolute -top-1  right-0 cursor-pointer  p-2 flex items-center justify-center '><i className='fa-solid fa-xmark text-text-color' ></i></button>
+                        </div>
+                        <div className='userData flex items-center mb-2'>
+                            <img className='w-10 h-10 rounded-full cursor-pointer object-cover object-top mr-3' src={userData?.photo ? userData.photo : profile} alt="" />
+                            <h2 className='text-text-color flex-1'>{userData?.name}</h2>
+                             <div className=' p-3 text-xl  rounded-lg  flex items-center justify-center w-fit'>
+                                <i className='fa-solid fa-image cursor-pointer text-green-500'></i>
+                            </div>
+                        </div>
+
+                        <form action="" className=''>
+                            <textarea name="" className=' w-full h-[200px] resize-none outline-none text-text-color' placeholder="Write something...."></textarea>
+
+                            <button className=' w-full bg text-white rounded-md p-2 cursor-pointer '>Post</button>
+                        </form>
+                    </div>
                 </div>
 
                 <div className='max-w-lg w-full overflow-x-auto  hide-scrollbar-x'>
-                <Stories />
-            </div>
+                    <Stories />
+                </div>
                 {allPosts?.slice().map((post, i) => (
                     <div key={i} className="container md:w-lg  bg-white dark:bg-gray-950 rounded-xl shadow-md">
 
