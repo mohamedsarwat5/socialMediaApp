a584c9de381e599ab25f540de777776ef7544f66
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-11-22 22:13:34 +0200

    somee

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -5,7 +5,7 @@ export default function Loading() {
     return (
         <div className='min-h-screen bg-bg-hero flex items-center justify-center   '>
              {/* <Logo className={'text-6xl animate-bounce text-pink'}/> */}
-             <div className=' text-6xl text-pink font-bold word'>
+             <div className=' text-6xl text-pink font-black word'>
                 <span>B</span>
                 <span>U</span>
                 <span>Z</span>
